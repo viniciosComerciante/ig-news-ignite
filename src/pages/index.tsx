1a011e81f07799147cd445a61ed589d3a4b04c0f
viniciosComerciante <vinicompif@gmail.com>
2021-11-09 01:44:57 -0300

    criado pagina principal, iniciando agora criaçao do botao de subscribe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/home.module.scss";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -7,7 +7,20 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1>Hello word</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
