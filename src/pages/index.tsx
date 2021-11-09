05929bf1b9ef2b914dd26b5f1397effca10dbdab
viniciosComerciante <vinicompif@gmail.com>
2021-11-09 01:22:55 -0300

    finalizado botao de login social com github

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
       <h1>Hello word</h1>
     </>
