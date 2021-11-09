6f20eb752721dc025d540e6cf50285969f56cdab
viniciosComerciante <vinicompif@gmail.com>
2021-11-09 00:16:35 -0300

    primeira parte concluida

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
+import Head from "next/head";
+import styles from "../styles/home.module.scss";
+
 export default function Home() {
-  return <h1>Hello word</h1>;
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1>Hello word</h1>
+    </>
+  );
 }
